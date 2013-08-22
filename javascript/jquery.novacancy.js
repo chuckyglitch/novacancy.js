2fdffc0f8c7f275f89f0dfc46901b26786afe9bf
Chuck Chang <chuck.chang@fanzytv.com>
2013-08-22 16:37:06 +0800

    version 0.3.1

--- javascript/jquery.novacancy.js
+++ javascript/jquery.novacancy.js
@@ -7,240 +7,236 @@
 * @twitter  <https://twitter.com/chuckyglitch>
 *
 * @repo https://github.com/chuckyglitch/novacancy.js
-* @version 0.3
+* @version 0.3.1
 * @license MIT http://opensource.org/licenses/MIT
-* @date 08-16-2013
+* @date 08-22-2013
 */
 
 ;(function($){
-	"use strict";
+    "use strict";
 
-	$.fn.novacancy = function(options){
-		var opts = $.extend({}, $.fn.novacancy.defaults, options);
-		return novacancy($(this), opts);
-	};
+    $.fn.novacancy = function(options){
 
-	function rand(min, max) {
-		return Math.floor(Math.random()*(max-min+1)+min);
-	}
+        /* parameters */
 
-	function isNumber(n) {
-		return !isNaN(parseFloat(n)) && isFinite(n);
-	}
+        var settings = $.extend({
+            'reblinkProbability': (1/3),
+            'blinkMin': 0.01,
+            'blinkMax': 0.5,
+            'loopMin': 0.5,
+            'loopMax': 2,
+            'color': 'ORANGE',
+            'glow': ['0 0 80px Orange', '0 0 30px Red', '0 0 6px Yellow'],
+            'off': 0,
+            'blink': 0,
+            'autoOn': true
+        }, options);
 
-	function blink(item, blinkMin, blinkMax, reblinkProbability) {
-		/* blink 1 time */
-		off(item);
-		item[0].blinking = true;
-		setTimeout(function() {
-			on(item);
-			item[0].blinking = false;
-			reblink(item, blinkMin, blinkMax, reblinkProbability);
-		}, rand(blinkMin, blinkMax) );
-	}
+        settings.reblinkProbability *= 100;
+        settings.blinkMin *= 1000;
+        settings.blinkMax *= 1000;
+        settings.loopMin *= 1000;
+        settings.loopMax *= 1000;
 
-	function reblink(item, blinkMin, blinkMax, reblinkProbability) {
-		setTimeout(function() {
-			/* continue blink check */
-			if (rand(1,100) <= reblinkProbability) {
-				blink(item, blinkMin, blinkMax, reblinkProbability);      
-			}
-		}, rand(blinkMin, blinkMax) );
-	}
+        /* */
 
-	function on(item) {
-		item.removeClass('off').addClass('on');
-	}
+        return novacancy($(this), settings);
+    };
 
-	function off(item) {
-		item.removeClass('on').addClass('off');
-	}
+    function rand(min, max) {
+        return Math.floor(Math.random()*(max-min+1)+min);
+    }
 
-	function novacancy(items, options) {
+    function isNumber(n) {
+        return !isNaN(parseFloat(n)) && isFinite(n);
+    }
 
-		items = $(items);
+    function blink(item, blinkMin, blinkMax, reblinkProbability) {
+        /* blink 1 time */
+        off(item);
+        item[0].blinking = true;
+        setTimeout(function() {
+            on(item);
+            item[0].blinking = false;
+            reblink(item, blinkMin, blinkMax, reblinkProbability);
+        }, rand(blinkMin, blinkMax) );
+    }
 
-		/* parameters */
+    function reblink(item, blinkMin, blinkMax, reblinkProbability) {
+        setTimeout(function() {
+            /* continue blink check */
+            if (rand(1,100) <= reblinkProbability) {
+                blink(item, blinkMin, blinkMax, reblinkProbability);      
+            }
+        }, rand(blinkMin, blinkMax) );
+    }
 
-		var _options = {
-			'reblinkProbability': (1/3),
-			'blinkMin': 0.01,
-			'blinkMax': 0.5,
-			'loopMin': 0.5,
-			'loopMax': 2,
-			'color': 'ORANGE',
-			'glow': ['0 0 80px Orange', '0 0 30px Red', '0 0 6px Yellow'],
-			'off': 0,
-			'blink': 0,
-			'autoOn': true
-		}
+    function on(item) {
+        item.removeClass('off').addClass('on');
+    }
 
-		$.each(options, function(index, value) {
-			_options[index] = value;
-		});
+    function off(item) {
+        item.removeClass('on').addClass('off');
+    }
 
-		_options.reblinkProbability *= 100;
-		_options.blinkMin *= 1000;
-		_options.blinkMax *= 1000;
-		_options.loopMin *= 1000;
-		_options.loopMax *= 1000;
+    function novacancy(items, settings) {
 
-		/* */
+        items = $(items);
 
-		var cssBuilder = '';
-		var cssBuildChecker = {};
+        /* */
 
-		$.each(items, function(index, value) {
+        var cssBuilder = '';
+        var cssBuildChecker = {};
 
-			var that = $(this);
-			var powerOn = false;
+        $.each(items, function(index, value) {
 
-			/* avoid repeat */
+            var that = $(this);
+            var powerOn = false;
 
-			if (that[0].novacancy) {
-				return true;
-			}
-			that[0].novacancy = true;
+            /* avoid repeat */
 
-			/* css string combine */
+            if (that[0].novacancy) {
+                return true;
+            }
+            that[0].novacancy = true;
 
-			var textShadow = 'text-shadow: '+_options.glow.toString()+';'
-			var colorOn = 'color: '+_options.color+';'+textShadow;
-			var colorOff = 'color: '+_options.color+'; opacity: 0.3;';
+            /* css string combine */
 
-			if (!cssBuildChecker[items.selector]) {
-				cssBuildChecker[items.selector] = 1;
-				cssBuilder += (items.selector+' .novacancy.on { '+colorOn+' }'+'\n');
-				cssBuilder += (items.selector+' .novacancy.off { '+colorOff+' }'+'\n');
-			}
+            var textShadow = 'text-shadow: '+settings.glow.toString()+';';
+            var colorOn = 'color: '+settings.color+';'+textShadow;
+            var colorOff = 'color: '+settings.color+'; opacity: 0.3;';
 
-			/* html string combine & rebuild to html */
+            if (!cssBuildChecker[items.selector]) {
+                cssBuildChecker[items.selector] = 1;
+                cssBuilder += (items.selector+' .novacancy.on { '+colorOn+' }'+'\n');
+                cssBuilder += (items.selector+' .novacancy.off { '+colorOff+' }'+'\n');
+            }
 
-			var htmlBuilder = '';
+            /* html string combine & rebuild to html */
 
-			$.each($(this).contents(), function(index, value) {
-				
-				if (value.nodeType == 3) {
-					var txts = value.nodeValue.split('');
-					$.each(txts, function(index, value) {
-						htmlBuilder += ( '<span class="novacancy on">'+value+'</span>' );
-					});
-				} else {
-					htmlBuilder += value.outerHTML;
-				}
+            var htmlBuilder = '';
 
-			});
+            $.each($(this).contents(), function(index, value) {
+                
+                if (value.nodeType == 3) {
+                    var txts = value.nodeValue.split('');
+                    $.each(txts, function(index, value) {
+                        htmlBuilder += ( '<span class="novacancy on">'+value+'</span>' );
+                    });
+                } else {
+                    htmlBuilder += value.outerHTML;
+                }
 
-			that.html(htmlBuilder);
+            });
 
-			/* */
+            that.html(htmlBuilder);
 
-			var len = that.find('span.novacancy').length;
-			var blinkArr = [];
-			var offArr = [];
-			var blinkArrLen;
-			var loopTimeout;
-			var arrLimit;
+            /* */
 
-			/* off make */
+            var len = that.find('span.novacancy').length;
+            var blinkArr = [];
+            var offArr = [];
+            var blinkArrLen;
+            var loopTimeout;
+            var arrLimit;
 
-			if (_options.off > 0) {
-				if (_options.off > len) _options.off = len;
-				for (var i = 1; i <= _options.off; i++) {
-					var num;
-					var item;
+            /* off make */
 
-					do {
-							num = rand(0, len-1);
-					} while ($.inArray(num, offArr) != -1);
+            if (settings.off > 0) {
+                if (settings.off > len) settings.off = len;
+                for (var i = 1; i <= settings.off; i++) {
+                    var num;
+                    var item;
 
-					offArr.push(num);
-					item = that.find('span.novacancy:eq('+num+')');
-					off(item);
-				}
-			} else {
-				_options.off = 0;
-			}
+                    do {
+                        num = rand(0, len-1);
+                    } while ($.inArray(num, offArr) != -1);
 
-			/* blink array make */
+                    offArr.push(num);
+                    item = that.find('span.novacancy:eq('+num+')');
+                    off(item);
+                }
+            } else {
+                settings.off = 0;
+            }
 
-			if (_options.blink > 0) {
-				if (_options.blink > len) _options.blink = len;
+            /* blink array make */
 
-				if ((_options.blink + _options.off) > len) {
-					_options.blink = _options.blink - _options.off;
-					if (_options.blink < 0) _options.blink = 0;
-				}
+            if (settings.blink > 0) {
+                if (settings.blink > len) settings.blink = len;
 
-				arrLimit = _options.blink;
-			} else {
-				arrLimit = len - _options.off;
-			}
+                if ((settings.blink + settings.off) > len) {
+                    settings.blink = settings.blink - settings.off;
+                    if (settings.blink < 0) settings.blink = 0;
+                }
 
-			for (i = 1; i<= arrLimit; i++) {
-				do {
-					num = rand(0, len-1);
-				} while ( ($.inArray(num, offArr) != -1) || ($.inArray(num, blinkArr) != -1) );
-				blinkArr.push(num);
-			}
+                arrLimit = settings.blink;
+            } else {
+                arrLimit = len - settings.off;
+            }
 
-			blinkArrLen = blinkArr.length;
+            for (i = 1; i<= arrLimit; i++) {
+                do {
+                    num = rand(0, len-1);
+                } while ( ($.inArray(num, offArr) != -1) || ($.inArray(num, blinkArr) != -1) );
+                blinkArr.push(num);
+            }
+
+            blinkArrLen = blinkArr.length;
 
 
-			/* blink loop */
+            /* blink loop */
 
-			function loop() {
-				if (!powerOn) return;
+            function loop() {
+                if (!powerOn) return;
 
-				var num;
-				var item;
+                var num;
+                var item;
 
-				num = blinkArr[rand(0, blinkArrLen-1)];
-				item = that.find('span.novacancy:eq('+num+')');                     
-				if (!item[0].blinking) blink(item, _options.blinkMin, _options.blinkMax, _options.reblinkProbability);
+                num = blinkArr[rand(0, blinkArrLen-1)];
+                item = that.find('span.novacancy:eq('+num+')');                     
+                if (!item[0].blinking) blink(item, settings.blinkMin, settings.blinkMax, settings.reblinkProbability);
 
-				loopTimeout = setTimeout(loop, rand(_options.loopMin, _options.loopMax) );
-			}
+                loopTimeout = setTimeout(loop, rand(settings.loopMin, settings.loopMax) );
+            }
 
-			function blinkOn() {
-				if (!powerOn) {
-					powerOn = true;
-					loopTimeout = setTimeout(loop, rand(_options.loopMin, _options.loopMax) );
-				}
-			}
+            function blinkOn() {
+                if (!powerOn) {
+                    powerOn = true;
+                    loopTimeout = setTimeout(loop, rand(settings.loopMin, settings.loopMax) );
+                }
+            }
 
-			function blinkOff() {
-				if (powerOn) {
-					powerOn = false;
-					clearTimeout(loopTimeout);
-				}
-			}
+            function blinkOff() {
+                if (powerOn) {
+                    powerOn = false;
+                    clearTimeout(loopTimeout);
+                }
+            }
 
-			if (_options.autoOn) blinkOn();
+            if (settings.autoOn) blinkOn();
 
-			/* events bind */
+            /* events bind */
 
-			$(this).on('blinkOn', function(e) {
-				blinkOn();
-			});
+            $(this).on('blinkOn', function(e) {
+                blinkOn();
+            });
 
-			$(this).on('blinkOff', function(e) {
-				blinkOff();
-			});
+            $(this).on('blinkOff', function(e) {
+                blinkOff();
+            });
 
-
-
-		});
+        });
 
 
-		/* css string append */
+        /* css string append */
 
-		var style = $('<style>'+cssBuilder+'</style>');
-		$('body').append(style);
+        var style = $('<style>'+cssBuilder+'</style>');
+        $('body').append(style);
 
-		/* */
+        /* */
 
-		return items;
-	}
+        return items;
+    }
 
 })(jQuery);
